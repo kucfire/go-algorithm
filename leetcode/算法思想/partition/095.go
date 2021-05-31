c59b0c6bd631c6a79b16d1bacb7b2a5371550b77
kucfire <cai972316027@qq.com>
2021-05-31 09:38:36 +0800

    update greedy

--- leetcode/算法思想/partition/095.go
+++ leetcode/算法思想/partition/095.go
@@ -16,9 +16,32 @@ func generateTrees(n int) []*TreeNode {
 	if n == 0 {
 		return nil
 	}
-	return
+	return getResult(1, n)
 }
 
 func getResult(start, end int) []*TreeNode {
+	if start > end {
+		return []*TreeNode{nil}
+	}
 
+	allTrees := []*TreeNode{}
+
+	// 枚举可行根节点
+	for i := start; i <= end; i++ {
+		// 获得所有可行的左子树集合
+		leftTree := getResult(start, i-1)
+		// 获得所有可行的右子树集合
+		rightTree := getResult(i+1, end)
+		// 从左子树集合中选出一颗左子树，从右子树结合中选出一颗右子树，拼接到节点上
+		for _, left := range leftTree {
+			for _, right := range rightTree {
+				currTree := &TreeNode{i, nil, nil}
+				currTree.Left = left
+				currTree.Right = right
+				allTrees = append(allTrees, currTree)
+			}
+		}
+	}
+
+	return allTrees
 }
---
+++ leetcode/算法思想/search/BFS/1091.go
@@ -0,0 +1,19 @@
+/*
+	leetcode tag:1091 title:二进制矩阵中的最短路径
+*/
+
+package bfs
+
+// method of bfs广度优先搜索
+func shortestPathBinaryMatrix(grid [][]int) int {
+	if len(grid) <= 0 {
+		return -1
+	}
+	list := []int{grid[0][0]}
+	result := 0
+	for len(list) > 0 {
+
+	}
+
+	return -1
+}
