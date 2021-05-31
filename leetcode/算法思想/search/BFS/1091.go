cc7c78762e6fea568f8e635d38d478aaf6c4f84b
kucfire <cai972316027@qq.com>
2021-05-31 17:26:21 +0800

    update greedy

--- leetcode/算法思想/search/BFS/1091.go
+++ leetcode/算法思想/search/BFS/1091.go
@@ -6,14 +6,61 @@ package bfs
 
 // method of bfs广度优先搜索
 func shortestPathBinaryMatrix(grid [][]int) int {
-	if len(grid) <= 0 {
+	if len(grid) <= 0 || grid[0][0] == 1 {
 		return -1
 	}
-	list := []int{grid[0][0]}
-	result := 0
+	list := [][]int{{0, 0, 1}} // 左边的位置 右边的位置 长度
+	// result := []int{}
 	for len(list) > 0 {
-
+		element := list[0]
+		list = list[1:]
+		// 校验是不是到达右下的单元格，如果是的话，返回element的长度，第一个遍历到证明就是最短路径
+		if element[0] == len(grid)-1 && element[1] == len(grid[0])-1 {
+			return element[2]
+		}
+		// 先校验可能的最短路径的点，即横纵坐标都+1的点
+		if element[0]+1 <= len(grid)-1 && element[1]+1 <= len(grid[0])-1 {
+			if grid[element[0]+1][element[1]+1] == 0 {
+				list = append(list, []int{element[0] + 1, element[1] + 1, element[2] + 1})
+				grid[element[0]+1][element[1]+1] = 1
+			}
+		}
+		// 校验可能的最短路径点的左和上两个临近当前点的点
+		if element[0]+1 <= len(grid)-1 {
+			if grid[element[0]+1][element[1]] == 0 {
+				list = append(list, []int{element[0] + 1, element[1], element[2] + 1})
+				grid[element[0]+1][element[1]] = 1
+			}
+		}
+		if element[1]+1 <= len(grid[0])-1 {
+			if grid[element[0]][element[1]+1] == 0 {
+				list = append(list, []int{element[0], element[1] + 1, element[2] + 1})
+				grid[element[0]][element[1]+1] = 1
+			}
+		}
+		// 校验最后的可能点
+		if element[0]+1 <= len(grid)-1 && element[1]-1 >= 0 {
+			if grid[element[0]+1][element[1]-1] == 0 {
+				list = append(list, []int{element[0] + 1, element[1] - 1, element[2] + 1})
+				grid[element[0]+1][element[1]-1] = 1
+			}
+		}
+		if element[1]+1 <= len(grid[0])-1 && element[0]-1 >= 0 {
+			if grid[element[0]-1][element[1]+1] == 0 {
+				list = append(list, []int{element[0] - 1, element[1] + 1, element[2] + 1})
+				grid[element[0]-1][element[1]+1] = 1
+			}
+		}
 	}
 
 	return -1
 }
+
+/*
+[0,1,1,0,0,0],
+[0,1,0,1,1,0],
+[0,1,1,0,1,0],
+[0,0,0,1,1,0],
+[1,1,1,1,1,0],
+[1,1,1,1,1,0]]
+*/
